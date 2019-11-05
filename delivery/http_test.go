3b55af6ddbfd92e2fc4e12a3f7c5e969dbfe6e09
Rakhmatullah Yoga S <rakhmatullahyoga@gmail.com>
2019-11-05 14:35:30 +0700

    covering route testing

--- delivery/http_test.go
+++ delivery/http_test.go
@@ -16,7 +16,7 @@ func TestHealthCheckHandler(t *testing.T) {
 
 	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
 	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(HealthCheckHandler)
+	handler := SetupHttpHandler()
 
 	// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
 	// directly and pass in our Request and ResponseRecorder.
