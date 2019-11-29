01f5480050e8580b81d6a29bda18252d0949f595
Rakhmatullah Yoga S <rakhmatullahyoga@gmail.com>
2019-11-29 17:33:21 +0700

    delete unnecessary custom error type

--- tigade.go
+++ tigade.go
@@ -1,6 +1,6 @@
 package tigade
 
-// Service model/schema
+// User entity
 type User struct {
 	Email       string
 	Password    string
@@ -9,13 +9,14 @@ type User struct {
 
 type Token string
 
-// Service contract
+// AccountService contract
 type AccountService interface {
 	Register(email string) error
 	ActivateUser(regToken Token) error
 	UpdateProfile(name string) error
 }
 
+// AuthService contract
 type AuthService interface {
 	Login(email, password string) (Token, error)
 	Logout(token Token) error
